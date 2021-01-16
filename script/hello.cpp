608ef16ec92b3d190aad7966c8d49dca6c3a481e
Azzam Shaikh <47488876+azzamshaikh@users.noreply.github.com>
2021-01-15 21:51:09 -0600

    Add files via upload

---
+++ script/hello.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to Azzam's World!\nThis world portrays a simple iteration of the 1st floor in my house.\nCurrently inside are a few obstacles and two robot models!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
